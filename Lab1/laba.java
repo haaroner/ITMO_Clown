477dd707677896e2522492d8bb74315d41129dbe
haaroner <yageo@bk.ru>
2025-09-26 23:58:03 +0300

    fixed proga laba

--- Lab1/laba.java
+++ Lab1/laba.java
@@ -2,20 +2,19 @@ import java.util.Random;
 
 public class laba 
 {
-	static int[] I1 = new int[6];
-	static double[][] I  = new double[6][20];
+	static int[] I = new int[6];
+	static double[][] l  = new double[6][20];
 	static float[] x = new float[20];
 	static double xj;
 	public static double calc(int i, int j, double _xj)
 	{
-		if(I1[i] == 9)
-			I[i][j] = Math.tan(Math.sin((_xj*(_xj+1))));
-		else if(I1[i] >= 11)
-			I[i][j] = Math.tan(Math.tan((Math.cos(_xj))));	
+		if(I[i] == 9)
+			l[i][j] = Math.tan(Math.sin(_xj*(_xj+1)));
+		else if(I[i] >= 11)
+			l[i][j] = Math.tan(Math.tan(Math.cos(_xj)));	
 		else
-			I[i][j] = Math.pow(((Math.pow(Math.atan(1D/Math.pow(Math.abs(Math.E), _xj)), 2D*2D*Math.pow(4D/_xj, 3D)))/
-			Math.cbrt(Math.tan(_xj)/(2D-Math.pow(Math.E, _xj)))+1D), 2D);
-		return I[i][j];
+			l[i][j] = Math.pow(((Math.pow(Math.atan(1D/Math.pow(Math.E, Math.abs(_xj) )), 2D*2D*Math.pow(4D/_xj, 3D)))/Math.cbrt(Math.tan(_xj)/(2D-Math.pow(Math.E, _xj)))+1D), 2D);
+		return l[i][j];
 	}
 
 	public static void printout()
@@ -37,7 +36,7 @@ public class laba
 		
 		for(int i = 17; i >= 7; i--) 
 			if(i%2 == 1)
-				I1[(17-i)/2] = i;
+				I[(17-i)/2] = i;
 
 		for(int i = 0; i < 20; i++)
 		{
